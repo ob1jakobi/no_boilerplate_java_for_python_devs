a3c8cbcab27a50bf67e3d99b03d697d9b9967b36
Jake McGrath <thejakezilla80@gmail.com>
2023-11-22 15:37:10 -0600

    Minor variable change

--- src/tech/jakemcgrath/no_boilerplate/from_python_to_java/PrimitiveTypesExamples.java
+++ src/tech/jakemcgrath/no_boilerplate/from_python_to_java/PrimitiveTypesExamples.java
@@ -8,7 +8,7 @@ public class PrimitiveTypesExamples {
         StringBuilder sb;
         String s;
 
-        n = 9;
+        n = 8;
         for (i = 0; i < 300; i += 10) {
             b = (byte) i;
             System.out.printf("%-5d%-7d", i, b);
@@ -16,11 +16,11 @@ public class PrimitiveTypesExamples {
             s = Integer.toBinaryString(i);
 
             start = Math.max(0, s.length() - n);
-            stop = Math.min(n - 1, s.length());
+            stop = Math.min(n, s.length());
 
             sb = new StringBuilder(s.substring(start, stop));
 
-            while (sb.length() < n - 1) {
+            while (sb.length() < n) {
                 sb.insert(0, "0");
             }
 
