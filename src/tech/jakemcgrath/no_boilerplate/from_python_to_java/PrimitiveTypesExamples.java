9fa06d4aa14ab1558fbabbb0532152615f364e05
Jake McGrath <thejakezilla80@gmail.com>
2023-11-22 17:28:42 -0600

    Removed useless code.

--- src/tech/jakemcgrath/no_boilerplate/from_python_to_java/PrimitiveTypesExamples.java
+++ src/tech/jakemcgrath/no_boilerplate/from_python_to_java/PrimitiveTypesExamples.java
@@ -4,23 +4,19 @@ package tech.jakemcgrath.no_boilerplate.from_python_to_java;
 public class PrimitiveTypesExamples {
     public static void main(String[] args) {
         byte b;
-        int i, n, start, stop;
+        int i, numBits;
         StringBuilder sb;
         String s;
 
-        n = 8;
+        numBits = 8;
         for (i = 0; i < 300; i += 10) {
             b = (byte) i;
             System.out.printf("%-5d%-7d", i, b);
 
             s = Integer.toBinaryString(Byte.toUnsignedInt(b));
+            sb = new StringBuilder(s);
 
-            start = Math.max(0, s.length() - n);
-            stop = Math.min(n, s.length());
-
-            sb = new StringBuilder(s.substring(start, stop));
-
-            while (sb.length() < n) {
+            while (sb.length() < numBits) {
                 sb.insert(0, "0");
             }
 
