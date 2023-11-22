fa92a44b582c3c1c066508341a17b037f7194583
Jake McGrath <thejakezilla80@gmail.com>
2023-11-22 15:31:35 -0600

    Example of code for binary representations of byte overflow.

---
+++ src/tech/jakemcgrath/no_boilerplate/from_python_to_java/PrimitiveTypesExamples.java
@@ -0,0 +1,31 @@
+package tech.jakemcgrath.no_boilerplate.from_python_to_java;
+
+
+public class PrimitiveTypesExamples {
+    public static void main(String[] args) {
+        byte b;
+        int i, n, start, stop;
+        StringBuilder sb;
+        String s;
+
+        n = 9;
+        for (i = 0; i < 300; i += 10) {
+            b = (byte) i;
+            System.out.printf("%-5d%-7d", i, b);
+
+            s = Integer.toBinaryString(i);
+
+            start = Math.max(0, s.length() - n);
+            stop = Math.min(n - 1, s.length());
+
+            sb = new StringBuilder(s.substring(start, stop));
+
+            while (sb.length() < n - 1) {
+                sb.insert(0, "0");
+            }
+
+            sb.insert(4, " ");
+            System.out.println(sb);
+        }
+    }
+}
